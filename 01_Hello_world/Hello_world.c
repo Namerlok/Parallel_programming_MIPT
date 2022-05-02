392b7df0996eca1dcdcec3784bb73d71ef65e8d4
Namerlok <Namerlok@yandex.ru>
2022-05-02 18:13:26 +0300

    Update first task

--- 01_Hello_world/Hello_world.c
+++ 01_Hello_world/Hello_world.c
@@ -4,14 +4,12 @@
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
     
-    int size = 0, rank = 0, name_len;
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int size = 0, rank = 0;
     
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Get_processor_name(processor_name, &name_len);
     
-    printf("Hell, world!\nprocessor %s, rank %d out of %d processors\n", processor_name, rank, size);
+    printf("Hell, world!\nrank %d out of %d processors\n", rank, size);
     
     MPI_Finalize();
     return 0;
